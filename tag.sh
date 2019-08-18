08ebf004d35b2d96bda38b8c97b0a44947669c2e
Jose Leon <leonj1@gmail.com>
2019-08-18 19:30:49 -0400

    fix syntax error

--- tag.sh
+++ tag.sh
@@ -15,9 +15,9 @@ if [ $# -eq 0 ]
     # replace . with space so can split into array
     VERSION_BITS=(${VERSION//./ })
     # get number of parts and increase the last one by 1
-    VNUM1=#{VERSION_BITS[0]}
-    VNUM2=#{VERSION_BITS[1]}
-    VNUM3=#{VERSION_BITS[2]}
+    VNUM1=${VERSION_BITS[0]}
+    VNUM2=${VERSION_BITS[1]}
+    VNUM3=${VERSION_BITS[2]}
     VNUM3=$((VNUM3+1))
     # create new tag
     NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
